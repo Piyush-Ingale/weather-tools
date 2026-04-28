63c79c021628d0ab1d4785f2a9d22d4c8f19ee96
Piyush-Ingale <piyush.ingale@infocusp.com>
2026-04-28 10:31:32 +0000

    test-commit

--- weather_mv/loader_pipeline/regrid.py
+++ weather_mv/loader_pipeline/regrid.py
@@ -37,7 +37,7 @@ logger = logging.getLogger(__name__)
 try:
     import metview as mv
     Fieldset = mv.bindings.Fieldset
-except (ModuleNotFoundError, ImportError, FileNotFoundError, ValueError):
+except (ModuleNotFoundError, ImportError, FileNotFoundError):
     logger.error('Metview could not be imported.')
     mv = None  # noqa
     Fieldset = t.Any
